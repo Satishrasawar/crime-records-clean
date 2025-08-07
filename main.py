c0303abfc2429a42ef714dd571870f012fdd572a
Satish Rasawar <satishrasawar@gmail.com>
2025-08-07 10:23:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from datetime import datetime
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # ADD DATABASE SECTION:
 database_ready = False
@@ -27,7 +28,13 @@ app = FastAPI(
     title="Client Records Data Entry System", 
     version="2.0.0"
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def root():
     return {
@@ -51,3 +58,4 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
